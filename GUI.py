7443af3bf9cf0744415697b1e18441d34db879d3
gatholocool <49301741+gatholocool@users.noreply.github.com>
2021-01-12 16:03:11 +0700

    ok

---
+++ GUI.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+from math import sin, cos, sqrt
+
+
+window = tk.Tk()
+
+greeting = tk.Label(text='Python Rock', fg = 'white', bg = 'black')
+greeting.pack()
+
+window.mainloop()
